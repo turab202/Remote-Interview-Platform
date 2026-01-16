df000c4c64e56171a34a52bb768b8cb6f00f6ab9
turab202 <turabturqb@gmail.com>
2026-01-16 14:43:28 +0300

    Authentication Setup - Clerk

--- interviewly/src/app/layout.tsx
+++ interviewly/src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,5 +39,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+   </ClerkProvider>
   );
 }
